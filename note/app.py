60e9adf185f1bb91cee6fd9c9a19c4a9cbfa8260
Martin Uribe <clamytoe@gmail.com>
2018-09-12 11:02:29 -0500

    Started building interactions with the database

--- note/app.py
+++ note/app.py
@@ -4,18 +4,42 @@ app.py
 
 Note taking utility
 """
-from .log_init import setup_logging
-from .parser import get_params
+from note import (db_add_note, db_check, db_connect, db_display_note, get_params,
+                  db_next_id, Note, setup_logging, db_get_note, db_view_notes)
 
 logger = setup_logging()
 
 
-def list_notes():
-    print("Listing notes")
+def add_note(note):
+    db_add_note(note)
+    print(f"  ID: {note.id}")
+    print(f"Note: {note.note}")
+    print(f"Tags: {', '.join(note.tags)}")
+    print(f"Date: {note.date}")
 
 
 def display_stats():
     print("Displaying stats")
+
+
+def edit_note(note_id):
+    _id, note, tags, date = db_get_note(note_id)
+    print(f"EDITING NOTE #{_id}:")
+    print(f"  (n)ote: {note}")
+    print(f"  (t)ags: {tags}")
+    print(f"  (d)ate: {date}")
+    choice = input("Which would you like to edit ([n]/t/d)? ")
+    if choice.lower().startswith("t"):
+        print(f"{tags}: ")
+    elif choice.lower().startswith("d"):
+        print(f"{date}: ")
+    else:
+        print(f"{note}: ")
+
+
+def list_notes(limit):
+    print("Listing notes")
+    db_view_notes(limit)
 
 
 def main():
@@ -23,6 +47,7 @@ def main():
 
     # note and tags
     note = params.get("note")
+    date = params.get("date")
     tags = params.get("tags")
 
     # edit note id
@@ -31,6 +56,7 @@ def main():
     # show notes and tags
     show_notes = params.get("show_notes")
     show_tags = params.get("show_tags")
+    show_limit = params.get("show_limit")
 
     # stats
     stats_all = params.get("stats_all")
@@ -39,20 +65,7 @@ def main():
     stats_month = params.get("stats_month")
     stats_year = params.get("stats_year")
 
-    if note:
-        print(f"NOTE: {note}")
-
-    if tags:
-        print(f"TAGS: {tags}")
-
-    if note_id:
-        print(f"EDIT: {note_id}")
-
-    if show_notes:
-        print(f"SHOW NOTES: {show_notes}")
-
-    if show_tags:
-        print(f"SHOW TAGS: {show_tags}")
+    tags = ", ".join(tags) if tags else None
 
     if stats_all:
         print(f"STATS ALL: {stats_all}")
@@ -68,6 +81,17 @@ def main():
 
     if stats_year:
         print(f"STATS ALL: {stats_year}")
+
+    if db_check():
+        if note:
+            new_note = Note(db_next_id(), note, tags, date)
+            add_note(new_note)
+
+        if show_notes:
+            list_notes(show_limit)
+
+        if note_id:
+            edit_note(note_id)
 
 
 if __name__ == "__main__":
