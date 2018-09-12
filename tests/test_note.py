bf002085c31a73c3e2b2c7e41621b355f35d218f
Martin Uribe <clamytoe@gmail.com>
2018-09-12 14:04:25 -0500

    Had to get at least one passing test in there!

--- tests/test_note.py
+++ tests/test_note.py
@@ -3,14 +3,19 @@ test_note.py
 
 Tests for note.
 """
+import pytest
 import logging
 
-from note import app
+from note import app, get_params
 
 logging.disable(logging.CRITICAL)
 
 
-def test_main(capfd):
-    app.main()
-    output = capfd.readouterr()[0]
-    assert output.strip() == "Successfully installed your project file: note"
+@pytest.fixture
+def parser():
+    return get_params()
+
+
+def test_main():
+    with pytest.raises(SystemExit):
+        app.main()
