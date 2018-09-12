2dc3effeeca07a11dad7632893231a3ebdfe589b
Martin Uribe <clamytoe@gmail.com>
2018-09-11 19:48:00 -0500

    Added database objects to the init file

--- note/__init__.py
+++ note/__init__.py
@@ -1,4 +1,6 @@
-from note import *
+from .db import add_note, check_db, db_connect, display_note, next_id, Note, view_notes
+from .log_init import setup_logging
+from .parser import get_params
 
 __author__ = 'Martin Uribe'
 __email__ = 'clamytoe@gmail.com'
