73e823343f29050b28d3bebfc793c54d9fb4008b
Martin Uribe <clamytoe@gmail.com>
2018-09-28 11:10:57 -0500

    Updated init file because lots of new functions were added and some were removed

--- note/__init__.py
+++ note/__init__.py
@@ -1,5 +1,7 @@
-from .db import (db_add_note, db_check, db_delete_note, db_next_id, Note,
-                 db_get_note, db_update_note, db_view_notes)
+from .db import (DATABASE, db_add_note, db_add_tag, db_add_tagged, db_connect,
+                 db_delete_note, db_delete_tag, db_get_all_tags, db_get_note,
+                 db_get_tag_id, db_get_tag, db_get_tag_name, db_get_tagged_by_note,
+                 db_get_tagged_by_tag, db_update_note, db_update_tag, db_view)
 from .log_init import setup_logging
 from .parser import get_params
 
